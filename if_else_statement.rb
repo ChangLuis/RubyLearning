6ddbf3319476925b9bcae53dbb452e06867e2e08
ChangLuis <somebodyforluis@gmail.com>
2019-08-07 09:59:32 +0800

    Add if else condition

---
+++ if_else_statement.rb
@@ -0,0 +1,14 @@
+check = false
+name = 'jack'
+
+if check && name
+  puts 'just say hi!'
+elsif check || name
+  puts 'go home jack!'
+else
+  puts 'good bye!'
+end
+
+# 都不用冒號， python的elif,在ruby則是elsif
+
+# and 傾向用 && ; or 傾向用 ||
