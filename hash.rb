e7e9273040392985054f25f3e0c35833bd8e9152
changluis <somebodyforluis@gmail.com>
2019-09-15 17:54:36 +0800

    Add max_by min_by and modified some notation

--- hash.rb
+++ hash.rb
@@ -11,8 +11,16 @@ puts dict1['a']
 dict3 = { a: 1, b: 2, c: 3 }
 puts dict3[:a]
 
-# 其實ruby也是有冒號的寫法，只不過他會變成符號形式
-# 呼叫的時候，前面要加上冒號．帶表示符號
+dict4 = { :a => 123, :b => 345, :c => 567 }
+puts dict4
+
+# 在ruby1.9版以後，有明確的規範一些風格，
+# 箭頭盡量不要用來當作符號的指示，當然是可以這樣表達，只不過風格不正確
+# 冒號可以用在key是string 或 symbol的情況
+
+p dict4[:d]
+# ruby的字典如果要輸出沒有的key 會出現nil 不會像python那樣出錯
+# 這在判斷式的使用比較方便 python就必須寫成例如 if 'firstname' in dict2
 
 dict2.each do |key, value|
   puts "This is the key - #{key} and the value - #{value}"
@@ -20,9 +28,9 @@ end
 
 dict1.each { |key, value| puts "This is the key - #{key} and the value - #{value}" }
 
-dict2.each { |key, value|
+dict2.each do |key, value|
   puts "The class of the key is #{key.class} and the class of the value is #{value.class}"
-}
+end
 
 dict2.delete('firstname')
 puts dict2
@@ -37,3 +45,11 @@ dict1.each { |k, v| dict1.delete(k) if v.is_a?(Integer) }
 puts dict1
 
 # .is_a?()是內部判別式，可以直接判別class
+
+people = { 'joe' => 21, 'bill' => 35, 'sally' => 24 }
+
+p people.min_by { |name, age| age }
+p people.max_by { |name, age| age }
+
+# 正常來講，hash要比較內容，通常要轉成array才能比較，不過ruby已經在eumerator max_by min_by 寫好method 你可以直接挪用就好
+# 而且max_by min_by 生成的array直接幫你把key and value搞成一對，真方便
