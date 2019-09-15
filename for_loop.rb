15f9278aabc64fdeb675acb2ba0b88142bc71856
changluis <somebodyforluis@gmail.com>
2019-09-15 17:55:36 +0800

    Add select,reduce method

--- for_loop.rb
+++ for_loop.rb
@@ -21,8 +21,24 @@ p array2.select { |num| num.odd? }
 
 # 其實要做一些判斷的 method都可以這樣寫
 
-array2 = array2.select { '&:odd?' }
-
+array2.select!(&:odd?)
+p array2
 # rubocop 傾向要我這樣寫
 
-p array2
+array3 = array2.select { :odd? }
+array4 = array2.select { '&:odd?' }
+# 其實也可以寫成上面這兩種
+p array3
+p array4
+
+# 會精簡成下面這些寫法，主要是block裡面的做的事情都是一樣的，可以不用再定義 |num| 這種情況
+
+p (1..50).to_a.reduce(:+)
+
+# 你一定會看過這種寫法，不過比較多應該看到的是inject(:+)
+# 但實際上是reduce inject這兩個method是一樣的功能
+# :+ 就是 + 囉，他的實際意義是
+p (1..50).to_a.reduce { |all_sum, num| all_sum += num }
+
+# 基本上就是一直加每個num,所以其實可以直接寫成&:+
+# 但ruby對這個&:+ 進一步優化成:+
